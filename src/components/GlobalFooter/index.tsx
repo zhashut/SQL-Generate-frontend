263889396aee3aa1ef0187ff95cd026a6f5aadcd
Zhashut <2105798885@qq.com>
2023-06-17 21:59:12 +0800

    first commit

---
+++ src/components/GlobalFooter/index.tsx
@@ -0,0 +1,59 @@
+import {
+  BugOutlined,
+  GithubOutlined,
+  SketchOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import React from 'react';
+import './index.less';
+
+/**
+ * 全局 Footer
+ *
+ * @author https://github.com/liyupi
+ */
+const GlobalFooter: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      className="default-footer"
+      copyright={`${currentYear} 炸薯条`}
+      links={[
+        {
+          key: 'master',
+          title: (
+            <>
+              <UserOutlined /> 站长：zhashut
+            </>
+          ),
+          href: 'https://space.bilibili.com/395185087',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: (
+            <>
+              <GithubOutlined /> 代码已开源
+            </>
+          ),
+          href: 'https://github.com/zhashut',
+          blankTarget: true,
+        },
+        {
+          key: 'feedback',
+          title: (
+            <>
+              <BugOutlined /> 建议反馈
+            </>
+          ),
+          href: 'https://support.qq.com/product/596724',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default GlobalFooter;
